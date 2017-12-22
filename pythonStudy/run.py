2be751999cd02d5132f7216a9476ade18bd073d5
jyYang <sky82e@naver.com>
2017-12-22 13:09:55 +0000

    First Commit

---
+++ pythonStudy/run.py
@@ -0,0 +1,6 @@
+import sys
+def openurl(url):
+     print(url)
+
+if __name__ == '__main__':
+    openurl(sys.argv[1])
---
+++ pythonStudy/test.py
@@ -0,0 +1,50 @@
+#-*- coding: utf-8 -*-
+
+def report(name,age,score):
+    print(name,score)
+
+report(age=10,name="yang",score=90)
+
+
+
+S="아리랑"
+print(S)
+S="""아리랑
+난리요"""
+
+print(S)
+
+a=1 #comment
+b=2
+c=4
+print( int(3.6) )
+
+import math
+import math
+if c > 3:
+    a=1
+    b=3
+    c=a+b
+else:
+    a=3
+    b=4
+    c=a+b
+
+print(c)
+
+import math
+n=math.sqrt(9.0)
+print(n )
+
+x=True
+y=False
+if x and y:
+    print("no")
+else:
+    print("yes")
+
+member=[1,2,3]
+mem=3 in member
+print(mem)
+print(4 in member)
+
